77e9e6c4a898728bef3d9c5ebcae409eba9ebef7
me.ca.fz@gmail.com <me.ca.fz@gmail.com>
2024-04-23 07:12:06 -0400

    footer

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -7,21 +7,47 @@ export default function Home() {
       <Container fluid>
         <Row>
           <Col>
-            <div style={{marginTop: "71px"}} className="img-container h-75 position-relative overflow-hidden d-none d-md-flex">
-              <img
-                className="w-100 h-55"
-                src="/images/pic.jpg"
-                alt="عکس"
-              />
+            <div
+              style={{ marginTop: "71px" }}
+              className="img-container h-75 position-relative overflow-hidden d-none d-md-flex"
+            >
+              <img className="w-100 h-55" src="/images/pic.jpg" alt="عکس" />
               <div className="img-overlay position-absolute top-0 start-0 w-100 h-100 bg-black bg-opacity-50"></div>
-              
-              
+
               <form className="d-flex justify-content-center search-form position-absolute top-50 start-50 vazir translate-middle">
-                <input type="search" placeholder="جستجو کنید..." className="form-control px-4 border-2 border-warning" />
-                <Button className="border-2 border-warning ms-1" variant="secondary">جستجو</Button>{' '}
+                <input
+                  type="search"
+                  placeholder="جستجو کنید..."
+                  className="form-control px-4 border-2 border-warning"
+                />
+                <Button
+                  className="border-2 border-warning ms-1"
+                  variant="secondary"
+                >
+                  جستجو
+                </Button>{" "}
               </form>
-             
             </div>
+          </Col>
+        </Row>
+        <Row className="vazir" xs={1} md={2}>
+          <Col xs={{ order: 2 }}>
+            <h2 className="mt-3">فروشگاه تکنوسام</h2>
+            <p style={{ textAlign: "justify", direction: "rtl" }} className="mt-2">
+              خرید آسان از تکنوسام, همراه با ضمانت بازگشت وجه و خرید اقساطی.
+              تمامی محصولات با پست پیشتاز ارسال می شوند.شما مشتریان گرامی
+              می‌توانید برای خرید اقساطی کالا مورد نظر خود در دسته بندی‌های
+              مختلف نظیر گوشی موبایل، لپ تاپ، تلویزیون، لوازم خانگی و سایر
+              کالاهای موجود در تکنوسام نسبت به ثبت نام در پلتفرم‌های همکار نظیر
+              ازکی وام، ایزی پی و بالن و دریافت اعتبار یا شارژ کیف پول خود اقدام
+              نمایید.پس از ثبت نام و دریافت اعتبار از پلتفرم‌های واسطه برای خرید
+              قسطی از تکنوسام, وارد حساب کاربری خود در سایت تکنوسام شوید. کالا
+              مورد نظر خود را انتخاب کرده و به سبد خرید خود اضافه کنید و فرایند
+              خرید خود را نهایی کنید.
+            </p>
+          </Col>
+          <Col xs={{ order: 1 }}>
+            <img src="/images/pin.jpg" className="img-fluid mt-5" />
           </Col>
         </Row>
       </Container>
--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -1,13 +1,91 @@
 import OffcanvasExample from "./Navbar";
+import "./footer.css";
 
-export default function Layout({children} : {children : React.ReactNode}) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div>
-        <header>
-            <OffcanvasExample />
-        </header>
-        <main>{children}</main>
-        <footer></footer>
+      <header>
+        <OffcanvasExample />
+      </header>
+      <main>{children}</main>
+      <footer className="vazir">
+        <div className="main">
+          <div className="logo row">
+            <div className="footer-header">
+
+              <h2>خدمات</h2>
+            </div>
+            <div className="logo-des">
+              <p>
+              تکنوسام فعالیت خود را از سال 1389 آغاز کرده است و تاکنون با اشتیاق و علاقه بالا آماده خدمت‌رسانی به مشتریانش است. برای اطلاعات بیشتر درباره‌ی نحوه شکل‌گیری تکنوسام می‌توانید به صفحه تاریخچه فعالیت تکنوسام مراجعه کنید.
+              </p>
+            </div>
+          </div>
+
+          <div className="office row">
+            <div className="footer-header">
+              <h3>پشتیبانی</h3>
+            </div>
+            <div className="office-des">
+              <p>
+               پشتیبانی فروشگاه اینترنتی تکنوسام
+              </p>
+              <a href="mailto:manikmaity.haker2003@gmail.com">
+                mehgdijafarazade848@gmail.com
+              </a>
+              <p className="num">9865214999+</p>
+            </div>
+          </div>
+
+          <div className="link row">
+            <div className="footer-header">
+              <h3>محصولات</h3>
+            </div>
+            <div className="link-des">
+              <a href="#" className="footer-links">
+                صفحه اصلی
+              </a>
+              <a href="#" className="footer-links">
+                موبایل
+              </a>
+              <a href="#" className="footer-links">
+                لپ تاپ
+              </a>
+              <a href="#" className="footer-links">
+                لوازم جانبی موبایل
+              </a>
+              <a href="#" className="footer-links">
+                لوازم جانبی کامپیوتر
+              </a>
+            </div>
+          </div>
+
+          <div className="newsletter row">
+            <div className="footer-header">
+              <h3>خبرنامه</h3>
+            </div>
+            <div className="newsletter-des">
+              <div className="subscribe">
+                <form>
+
+                <input
+                  type="email"
+                  placeholder="Enter Email ID"
+                  required
+                  className="border rouanded-sm px-1 py-1"
+                />
+                <button className="px-4 py-1 btn-know">ارسال</button>
+                </form>
+
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="copyright">
+          <hr />
+          <p>© حقوق این وب سایت برای تکنوسام محفوظ می باشد.</p>
+        </div>
+      </footer>
     </div>
-  )
+  );
 }
